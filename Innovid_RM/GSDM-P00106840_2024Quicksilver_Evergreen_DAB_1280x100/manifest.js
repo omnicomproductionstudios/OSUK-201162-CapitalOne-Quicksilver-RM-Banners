b005458d8673987122956d30d29db8807d1cacf9
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2025-11-14 15:11:26 +0530

    dfjvb

---
+++ Innovid_RM/GSDM-P00106840_2024Quicksilver_Evergreen_DAB_1280x100/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+	"filename":"GSDM-P00106840_2024Quicksilver_Evergreen_DAB_1280x100.html",
+	"width":1280,
+	"height":100,
+	"clickTagCount":1,
+  "videos":[{"name":"video1", "ref":"208574/TURW4454000H_P00105655_QUICKSILVER_HOLIDAYNIGHTFEVER_REV2_CFP_OLV_15_HD"}],
+	"expand":{
+		"fullscreen":false,
+		"width":1280,
+		"height":418,
+		"indentAcross":0,
+		"indentDown":0
+  }
+});
---
+++ Innovid_RM/GSDM-P00106840_2024Quicksilver_Evergreen_DAB_970x66/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+	"filename":"GSDM-P00106840_2024Quicksilver_Evergreen_DAB_970x66.html",
+	"width":970,
+	"height":66,
+	"clickTagCount":1,
+  "videos":[{"name":"video1", "ref":"208574/TURW4454000H_P00105655_QUICKSILVER_HOLIDAYNIGHTFEVER_REV2_CFP_OLV_15_HD"}],
+	"expand":{
+		"fullscreen":false,
+		"width":970,
+		"height":250,
+		"indentAcross":0,
+		"indentDown":0
+  }
+});
